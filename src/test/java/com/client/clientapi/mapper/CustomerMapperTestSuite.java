d0770379d03329a7ffd612a2cd3d02680fd6d1b2
crycald <krzysiekdybikowski@gmail.com>
2020-06-02 13:44:32 +0200

    mapper test for customer

---
+++ src/test/java/com/client/clientapi/mapper/CustomerMapperTestSuite.java
@@ -0,0 +1,56 @@
+package com.client.clientapi.mapper;
+
+import com.client.clientapi.domain.Customer;
+import com.client.clientapi.domain.CustomerDto;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerMapperTestSuite {
+    private CustomerMapper mapper = new CustomerMapper();
+    @Test
+    public void testDtoToMap() {
+        CustomerDto customerDto = new CustomerDto(1L, "Chris", "Mops", "pwd123", "mail@mail.com", "123-123-123");
+        Customer customer= mapper.map(customerDto);
+
+        Assert.assertEquals(customer.getId(), customerDto.getId());
+        Assert.assertEquals(customer.getFirstname(), customerDto.getFirstname());
+        Assert.assertEquals(customer.getLastname(), customerDto.getLastname());
+        Assert.assertEquals(customer.getEmail(), customerDto.getEmail());
+        Assert.assertEquals(customer.getPhoneNumber(), customerDto.getPhoneNumber());
+        Assert.assertEquals(customer.getPassword(), customerDto.getPassword());
+    }
+
+    @Test
+    public void testMapToDto() {
+        Customer customer = new Customer(1L, "Chris", "Mops", "pwd123", "mail@mail.com", "123-123-123");
+        CustomerDto customerDto = mapper.mapToDto(customer);
+
+        Assert.assertEquals(customerDto.getId(), customer.getId());
+        Assert.assertEquals(customerDto.getFirstname(), customer.getFirstname());
+        Assert.assertEquals(customerDto.getLastname(), customer.getLastname());
+        Assert.assertEquals(customerDto.getEmail(), customer.getEmail());
+        Assert.assertEquals(customerDto.getPhoneNumber(), customer.getPhoneNumber());
+        Assert.assertEquals(customerDto.getPassword(), customer.getPassword());
+    }
+
+    @Test
+    public void testToList() {
+        Customer customer1 = new Customer(1L, "Chris", "Mops", "pwd123", "mail@mail.com", "123-123-123");
+        Customer customer2 = new Customer(1L, "Chris", "Mops", "pwd123", "mail@mail.com", "123-123-123");
+        Customer customer3 = new Customer(1L, "Chris", "Mops", "pwd123", "mail@mail.com", "123-123-123");
+        Customer customer4 = new Customer(1L, "Chris", "Mops", "pwd123", "mail@mail.com", "123-123-123");
+
+        List<Customer> customerList = new ArrayList<>();
+        customerList.add(customer1);
+        customerList.add(customer2);
+        customerList.add(customer3);
+        customerList.add(customer4);
+
+        List<CustomerDto> customerDtoList = mapper.list(customerList);
+
+        Assert.assertEquals(4, customerDtoList.size());
+    }
+}
