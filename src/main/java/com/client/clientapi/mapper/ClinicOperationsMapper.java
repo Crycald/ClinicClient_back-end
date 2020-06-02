96ecd8dc9fa18ffa245ac63db93cc16c2a1e4be4
crycald <krzysiekdybikowski@gmail.com>
2020-06-02 13:35:21 +0200

    added mapper for specialization

---
+++ src/main/java/com/client/clientapi/mapper/ClinicOperationsMapper.java
@@ -0,0 +1,33 @@
+package com.client.clientapi.mapper;
+
+import com.client.clientapi.domain.ClinicOperations;
+import com.client.clientapi.domain.ClinicOperationsDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ClinicOperationsMapper {
+    public ClinicOperations map(final ClinicOperationsDto clinicOperationsDto) {
+        return ClinicOperations.builder()
+                .id(clinicOperationsDto.getId())
+                .clinic_id(clinicOperationsDto.getClinic_id())
+                .operations(clinicOperationsDto.getOperations())
+                .cost(clinicOperationsDto.getCost())
+                .build();
+    }
+
+    public ClinicOperationsDto mapToDto(final ClinicOperations clinicOperations) {
+        return new ClinicOperationsDto(
+                clinicOperations.getId(),
+                clinicOperations.getClinic_id(),
+                clinicOperations.getOperations(),
+                clinicOperations.getCost()
+        );
+    }
+
+    public List<ClinicOperationsDto> list(final List<ClinicOperations> clinicOperationsList) {
+        return clinicOperationsList.stream()
+                .map(this::mapToDto)
+                .collect(Collectors.toList());
+    }
+}
