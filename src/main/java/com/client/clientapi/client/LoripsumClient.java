485710e9539f595eecc9be12fda05fc84e102bcc
crycald <krzysiekdybikowski@gmail.com>
2020-06-25 19:26:56 +0200

    try/catch to loripsum

--- src/main/java/com/client/clientapi/client/LoripsumClient.java
+++ src/main/java/com/client/clientapi/client/LoripsumClient.java
@@ -4,13 +4,14 @@ import com.client.clientapi.domain.api.LoripsumDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class LoripsumClient {
     @Value("${loripsum.api.endpoint.prod}")
     private String loripsumApiEndPoint;
-    private final RestTemplate restTemplate;
+    private RestTemplate restTemplate;
     private LoripsumDto loripsumDto = new LoripsumDto();
 
     @Autowired
@@ -23,8 +24,14 @@ public class LoripsumClient {
         loripsumDto.setTextType("plaintext");
         loripsumDto.setParagraphs(3);
 
-        loripsumDto.setContent(restTemplate.getForObject(loripsumApiEndPoint + "/" + loripsumDto.getTextLength() +
-                "/" + loripsumDto.getTextType() + "/" + loripsumDto.getParagraphs(), String.class));
+        try {
+            loripsumDto.setContent(restTemplate.getForObject(loripsumApiEndPoint + "/" + loripsumDto.getTextLength() +
+                    "/" + loripsumDto.getTextType() + "/" + loripsumDto.getParagraphs(), String.class));
+
+        } catch (RestClientException e) {
+            System.out.println(e);
+            loripsumDto.setContent("There was an error with request to the API...");
+        }
 
         return loripsumDto.getContent();
     }
